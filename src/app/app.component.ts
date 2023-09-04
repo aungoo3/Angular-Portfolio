d5dc91fdb16d119248626f2578200520ef85fe18
Aung Oo <aungtechpro@gmail.com>
2023-09-04 18:06:47 -0400

    First Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title:string = 'Aung Oo';
+  city : string = "Atlanta, Georgia, USA";
+  tagline: string = "It is worth die doing it!";
+  aboutMe: string = "My name is Aung, I am from Atlanta, Georgia, USA. I have coding and software enginreering. Becoming a professional software engineer is my long term goal.";
+}
